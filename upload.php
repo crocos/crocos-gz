b935666ca02330cf0da2eab00e1e25925dcc5ca8
Sotaro KARASAWA <sotarok@crocos.co.jp>
2011-12-01 15:43:23 +0900

    add php

---
+++ upload.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ *
+ */
+
+//$hash = md5($_POST['imagedata']);
+$hash = md5($_POST['imagedata']);
+$dir = date("YmdHis");
+$fname = $dir . '/' . $hash . '.png';
+if (!is_dir("data/{$dir}")) {
+    mkdir("data/{$dir}");
+}
+file_put_contents("data/{$fname}", $_POST['imagedata']);
+$db_filename = "db/id.db";
+$db = unserialize(file_get_contents($db_filename));
+$db[$hash] = $_POST['id'];
+file_put_contents("db/id.db", serialize($db));
+echo "http://g.dev.crocos.jp/data/$fname";
